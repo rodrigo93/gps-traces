c9ba67226d628c28aeb5e9576c57d167d0ec0ae2
Rodrigo Marques <rodrigomarques1993@gmail.com>
2020-02-06 23:26:23 -0300

    New DistanceUpdater service

--- app/models/trace.rb
+++ app/models/trace.rb
@@ -9,40 +9,19 @@ class Trace < ApplicationRecord
     JSON.parse(coordinates)&.each(&:deep_symbolize_keys!)
   end
 
+  def distances_calculated?
+    coordinates_as_hash.each { |coordinate| coordinate.has_key?(:distance) }
+  end
+
   private
 
   def check_coordinates
-    coordinates_as_hash.each do |coordinate|
-      raise StandardError if Coordinate.new(coordinate).invalid?
-    end
+    coordinates_as_hash.each { |coordinate| raise StandardError if Coordinate.new(coordinate).invalid? }
   rescue StandardError
     errors.add(:coordinates, 'The JSON must be well formatted and all latitudes and longitudes must be present')
   end
 
-  # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
   def update_distances
-    new_coordinates = coordinates_as_hash
-
-    new_coordinates.each_with_index do |coordinate, index|
-      if index.zero?
-        new_coordinates[index][:distance] = 0
-        next
-      end
-
-      previous_coordinate = new_coordinates[index-1]
-      distance = distance_calculator.distance_between_coordinates(previous_coordinate[:latitude],
-                                                                  previous_coordinate[:longitude],
-                                                                  coordinate[:latitude],
-                                                                  coordinate[:longitude])
-
-      new_coordinates[index][:distance] = (distance + previous_coordinate[:distance]).ceil
-    end
-
-    self.coordinates = new_coordinates.to_json
-  end
-  # rubocop:enable Metrics/AbcSize, Metrics/MethodLength
-
-  def distance_calculator
-    @distance_calculator ||= CalculatorService
+    TracesServices::DistanceUpdater.new(self).call
   end
 end
---
+++ app/services/traces_services/distance_updater.rb
@@ -0,0 +1,39 @@
+module TracesServices
+  class DistanceUpdater
+    def initialize(trace)
+      @trace = trace
+      @new_coordinates ||= trace.coordinates_as_hash
+      @distance_calculator ||= CalculatorService
+    end
+
+    def call
+      calculate_coordinates_distances
+      update_trace
+    end
+
+    private
+
+    attr_accessor :trace, :new_coordinates, :distance_calculator
+
+    def calculate_coordinates_distances
+      new_coordinates.each_with_index do |coordinate, index|
+        if index.zero?
+          new_coordinates[index][:distance] = 0
+          next
+        end
+
+        previous_coordinate = new_coordinates[index-1]
+        distance = distance_calculator.distance_between_coordinates(previous_coordinate[:latitude],
+                                                                    previous_coordinate[:longitude],
+                                                                    coordinate[:latitude],
+                                                                    coordinate[:longitude])
+
+        new_coordinates[index][:distance] = (distance + previous_coordinate[:distance]).ceil
+      end
+    end
+
+    def update_trace
+      @trace.coordinates = new_coordinates.to_json
+    end
+  end
+end
